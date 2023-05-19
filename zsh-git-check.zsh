b984e212bb86b037d8b10c6507f22651cf46bb89
git_girl <lotta.stefanova@posteo.de>
2023-05-19 10:00:45 +0200

    init project from ppp, started startup sourcing script

---
+++ zsh-git-check.zsh
@@ -0,0 +1,44 @@
+#!/usr/bin/env zsh
+
+__git_check_read_origin_and_fetch() { 
+  # returns 124 on timeout to $?
+  # returns 127 on command not known to $?
+  timeout 3 git fetch 
+
+  retval=$?
+
+  if [[ $retval != 0 ]]; then 
+    notify-send "Git Fetch & Diff" "couldn't run git fetch (3sec timeout), check your ssh keys, or internet connection"
+  fi
+
+  return $retval
+}
+
+__run_and_report_git_diff() { 
+
+  git_diff=$(git diff @{u} HEAD --name-only)
+  if [[ $git_diff != "" ]]; then
+    notify-send "Git Fetch & Diff" "Found these files on origin different to local: \n $git_diff"
+  else 
+    notify-send "Git Fetch & Diff" "Clean: no diff to origin isn't ahead of local"
+  fi  
+}
+
+__git_main() { 
+  git_check_read_origin_and_fetch
+
+  access=$?
+
+  if [ $access -eq 0 ]; then 
+    run_and_report_git_diff >/dev/null & disown
+  fi
+}
+
+__git_check_hook_fn() { 
+  if [[ -d './.git' ]]; then
+    (git_main 1> /dev/null & disown)
+  fi
+}
+
+# add with: 
+# add-zsh-hook chpwd git_check_hook_fn
